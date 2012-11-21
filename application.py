1d32a671bbff00ebb7fb8f59109ef8b4d0fa7512
Rino Jose <rjose@linkedin.com>
2012-11-21 13:05:47 -0800

    Added initial template for rendering a list of projects

--- application.py
+++ application.py
@@ -11,7 +11,10 @@ def root():
 def projects():
     # TODO: Simulate getting data for different groupings and rendering it
     print '==> %s' % request.args.get('group', None)
-    return render_template('projects.html')
+    search_project = {'title': 'Search', 'target_date': 'Feb 15, 2012', 'est_date': 'Feb 10, 2012',
+        'detail_url': '/projects/1',
+        'key_dates': [['Integration with PAL', 'Dec 15, 2012']]}
+    return render_template('projects.html', projects=[search_project])
 
 @app.route('/projects/<int:project_id>')
 def project(project_id):
