dd00c16377637628b5db50ef6684070e1c1515e0
Rino Jose <rjose@ejorp.com>
2012-11-20 23:05:22 -0800

    Added some initial routes and dummy content

---
+++ application.py
@@ -0,0 +1,33 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def root():
+    return 'Root page'
+
+# Projects
+@app.route('/projects')
+def projects():
+    return 'List of all Projects'
+
+@app.route('/projects/<int:project_id>')
+def project(project_id):
+    return 'Details for project %s' % project_id
+
+@app.route('/projects/new')
+def projects_new():
+    return 'Create a new project'
+
+# People
+@app.route('/people')
+def people():
+    return 'List of all People'
+
+@app.route('/people/new')
+def people_new():
+    return 'Create new person'
+
+if __name__ == '__main__':
+    # TODO: Figure out how to set this via an environment variable
+    app.run(debug=True)
+
