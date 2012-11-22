4ac1d729e6139f067642462003dac0feb6a2cca7
Rino Jose <rjose@ejorp.com>
2012-11-22 09:24:46 -0800

    Mocked: adding work to a project

--- application.py
+++ application.py
@@ -99,6 +99,18 @@ def participants_new():
 
 
 # Work
+@app.route('/work', methods=['GET', 'POST'])
+def work():
+    if request.method == 'POST':
+        # TODO: Implement the creation of the work
+        print "==> Work name: %s" % request.form['name']
+        print "==> Work assignee: %s" % request.form['assignee']
+        print "==> Work estimate: %s" % request.form['estimate']
+        project_id = request.args.get('project', None)
+        return redirect(url_for('project', project_id=project_id))
+    else:
+        return "TODO: Figure out what should go here"
+
 @app.route('/work/edit')
 def work_edit():
     project_id = request.args.get('project', None)
@@ -106,8 +118,11 @@ def work_edit():
 
 @app.route('/work/new')
 def work_new():
-    project_id = request.args.get('project', None)
-    return "Add work to project %s" % project_id
+    project_id = request.args.get('project', 0)
+
+    # TODO: Do some error handling if we don't have a project id
+    project = {'project_name': 'Endorsements', 'project_id': project_id}
+    return render_template('work_new.html', project=project)
 
 if __name__ == '__main__':
     # TODO: Figure out how to set this via an environment variable
