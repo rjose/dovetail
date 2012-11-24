3028ea301ee998f8a54ee2e9dee2404fb28e9860
Rino Jose <rjose@ejorp.com>
2012-11-24 07:40:14 -0800

    Reformat: added space before '='

--- dovetail/database.py
+++ dovetail/database.py
@@ -1,27 +1,27 @@
 from datetime import datetime
 from sqlalchemy import (create_engine, MetaData, Table, Column,
-    Integer, String, Date)
+    Integer, String, Date, Float)
 
 # TODO: Use an environment/config variable for this
 engine = create_engine('sqlite:////Users/rjose/database/dovetail_dev.db',
         convert_unicode=True)
 metadata = MetaData(bind=engine)
 
-people= Table('people', metadata,
+people = Table('people', metadata,
         Column('id', Integer, primary_key=True),
         Column('name', String(50)),
         Column('picture', String(200)),
         Column('team', String(200)),
         Column('title', String(200)))
 
-projects= Table('projects', metadata,
+projects = Table('projects', metadata,
         Column('id', Integer, primary_key=True),
         Column('name', String(50)),
         Column('target_date', Date()),
         Column('est_date', Date())
         )
 
-project_participants= Table('project_participants', metadata,
+project_participants = Table('project_participants', metadata,
         Column('project_id', Integer),
         Column('person_id', Integer)
         )
