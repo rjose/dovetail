86c48e53bdef4d7dc2fcd047ee3bc7937fccb70a
Rino Jose <rjose@ejorp.com>
2012-11-27 12:21:27 -0800

    TDD: Work knows earliest start date

--- dovetail/tests/test_work.py
+++ dovetail/tests/test_work.py
@@ -3,7 +3,7 @@ from mock import MagicMock
 from datetime import datetime
 from dovetail.timeline.timeline import Timeline
 from dovetail.timeline.slot import Slot
-from dovetail.work.work import Work
+from dovetail.work.work import Work, earliest_start_date
 
 class TestWork(unittest.TestCase):
 
@@ -20,3 +20,21 @@ class TestWork(unittest.TestCase):
         self.assertEqual(nov8, w.est_end_date())
         return
 
+    def test_earliest_start_date(self):
+        nov1 = datetime.strptime("Nov 1, 2012", "%b %d, %Y") # Thu
+        nov5 = datetime.strptime("Nov 5, 2012", "%b %d, %Y") # Mon
+        nov7 = datetime.strptime("Nov 7, 2012", "%b %d, %Y") # Wed
+        timeline = Timeline(nov1)
+        work_slot, _ = timeline.find_slot(4, 1.5)
+
+        w1 = Work(1, 'w1', 1.0, [], 100, None)
+        w1.schedule(timeline.find_slot(0, 1)[0])
+
+        w2 = Work(2, 'w2', 1.0, [], 101, None)
+        w2.schedule(timeline.find_slot(1, 3)[0])
+
+        w3 = Work(3, 'w3', 1.0, [1, 2], 200, None)
+
+        work_dict = {1: w1, 2: w2, 3: w3}
+        self.assertEqual(nov7, earliest_start_date(w3, work_dict, nov1))
+        return
--- dovetail/work/work.py
+++ dovetail/work/work.py
@@ -1,3 +1,11 @@
+def earliest_start_date(work, work_dict, cur_day):
+    result = cur_day
+    for work_id in work.prereqs:
+        end_date = work_dict[work_id].est_end_date()
+        if end_date and end_date > result:
+            result = end_date
+    return result
+
 class Work():
     
     def __init__(self, work_id, title, effort_left_d, prereqs, assignee_id, key_date):
