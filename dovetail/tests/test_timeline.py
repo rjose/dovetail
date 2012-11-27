914dcdfffeb0672ae177b51a189c39596c4fda09
Rino Jose <rjose@ejorp.com>
2012-11-26 23:55:00 -0800

    TDD: Timeline.get_day_from_date

--- dovetail/tests/test_timeline.py
+++ dovetail/tests/test_timeline.py
@@ -22,6 +22,45 @@ class TestTimeline(unittest.TestCase):
         timeline = Timeline(datetime.now())
         timeline.claim_slot(Slot(4, 4.5), 0)
         self.assertEqual([Slot(0, 4), Slot(4.5, float('inf'))], timeline.slots)
+        return
+
+    def test_add_dates(self):
+        nov1 = datetime.strptime("Nov 1, 2012", "%b %d, %Y") # Thu
+        nov2 = datetime.strptime("Nov 2, 2012", "%b %d, %Y") # Fri
+        nov3 = datetime.strptime("Nov 3, 2012", "%b %d, %Y") # Sat
+        nov5 = datetime.strptime("Nov 5, 2012", "%b %d, %Y") # Mon
+
+        timeline = Timeline(nov1)
+        timeline.add_dates(nov1)
+        self.assertEqual([nov1], timeline.dates)
+
+        timeline.add_dates(nov2)
+        self.assertEqual([nov1, nov2], timeline.dates)
+
+        # Check idempotent
+        timeline.add_dates(nov2)
+        self.assertEqual([nov1, nov2], timeline.dates)
+
+        # Skip until next workday
+        timeline.add_dates(nov3)
+        self.assertEqual([nov1, nov2, nov5], timeline.dates)
+        return
+
+    def test_day_from_date(self):
+        nov1 = datetime.strptime("Nov 1, 2012", "%b %d, %Y") # Thu
+        nov2 = datetime.strptime("Nov 2, 2012", "%b %d, %Y") # Fri
+        nov3 = datetime.strptime("Nov 3, 2012", "%b %d, %Y") # Sat
+        nov4 = datetime.strptime("Nov 4, 2012", "%b %d, %Y") # Sun
+        nov5 = datetime.strptime("Nov 5, 2012", "%b %d, %Y") # Mon
+
+        timeline = Timeline(nov1)
+        self.assertEqual(0, timeline.day_from_date(nov1))
+        self.assertEqual(1, timeline.day_from_date(nov2))
+        self.assertEqual(2, timeline.day_from_date(nov3))
+        self.assertEqual(2, timeline.day_from_date(nov4))
+        self.assertEqual(2, timeline.day_from_date(nov5))
+        return
+
         
 if __name__ == '__main__':
     unittest.main()
--- dovetail/timeline/timeline.py
+++ dovetail/timeline/timeline.py
@@ -1,4 +1,5 @@
 from dovetail.timeline.slot import Slot
+from datetime import timedelta
 
 def update_days(start_day, width):
     return start_day, start_day + width
@@ -8,6 +9,7 @@ class Timeline():
     def __init__(self, cur_date):
         self.slots = [Slot(0, float('inf'))]
         self.cur_date = cur_date
+        self.dates = []
 
     def find_slot(self, earliest_start_day, width):
         new_slot = None
@@ -33,4 +35,50 @@ class Timeline():
         self.slots = self.slots[:index] + new_slots + self.slots[index+1:]
         return slot
 
+    def day_from_date(self, date):
+        if date < self.cur_date:
+            return None
+        if self.need_to_add_dates(date):
+            self.add_dates(date)
+
+        # Keep incrementing date until we find one in the timeline.
+        # This is guaranteed because add_dates will add at least one date
+        # at or after the specified date.
+        delta1 = timedelta(1) # 1 day
+        while not date in self.dates:
+            date += delta1
+        return self.dates.index(date)
+
+    def need_to_add_dates(self, date):
+        return self.dates == [] or date > self.dates[-1]
+
+    def is_workday(self, date):
+        # TODO: Check holiday or OOO
+        result = True
+        if date.weekday() in [5, 6]:
+            result = False
+        else:
+            result = True
+        return result
+
+    def add_dates(self, end_date):
+        if end_date < self.cur_date:
+            return
+
+        has_added_date = False
+        delta1 = timedelta(1) # 1 day
+        date = None
+        if self.dates == []:
+            date = self.cur_date - delta1
+        else:
+            date = self.dates[-1]
+
+        while date < end_date:
+            date += delta1
+            if self.is_workday(date):
+                self.dates.append(date)
+                has_added_date = True
+            elif date == end_date and has_added_date == False:
+                end_date += delta1
+        return
 
