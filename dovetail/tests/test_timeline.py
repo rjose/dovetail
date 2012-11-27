4be2dab749bf09ddd686b753ae58d5fd84a1862c
Rino Jose <rjose@linkedin.com>
2012-11-26 16:53:54 -0800

    Added: Test to show that timeline can find a slot that's not a preferred slot

--- dovetail/tests/test_timeline.py
+++ dovetail/tests/test_timeline.py
@@ -12,7 +12,11 @@ class TestTimeline(unittest.TestCase):
         self.assertEqual(Slot(4, 4.5), my_slot)
         self.assertEqual(0, containing_slot_index)
 
-        # TODO: Test where prefered slot is not available
+        # Test where prefered slot is not available
+        timeline.slots = [Slot(1, 3), Slot(4, float('inf'))]
+        my_slot, containing_slot_index = timeline.find_slot(0, 2.5)
+        self.assertEqual(Slot(4, 6.5), my_slot)
+        self.assertEqual(1, containing_slot_index)
 
     def test_claim_slot(self):
         timeline = Timeline(datetime.now())
