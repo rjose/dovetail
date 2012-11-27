e1f3522de666865f22ec12fe27a54ab1cde8743d
Rino Jose <rjose@linkedin.com>
2012-11-26 16:54:22 -0800

    Added: test suites for Slot and Timeline

--- run_tests.py
+++ run_tests.py
@@ -1,5 +1,10 @@
 import unittest
 from dovetail.tests.test_edit_project_work import TestEditProjectWork
+from dovetail.tests.test_slot import TestSlot
+from dovetail.tests.test_timeline import TestTimeline
 
-suite = unittest.TestLoader().loadTestsFromTestCase(TestEditProjectWork)
-unittest.TextTestRunner(verbosity=2).run(suite)
+suite_edit_project_work = unittest.TestLoader().loadTestsFromTestCase(TestEditProjectWork)
+suite_slot = unittest.TestLoader().loadTestsFromTestCase(TestSlot)
+suite_timeline = unittest.TestLoader().loadTestsFromTestCase(TestTimeline)
+
+unittest.TextTestRunner(verbosity=2).run(suite_timeline)
