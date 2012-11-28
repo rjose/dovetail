0227f20bbd244716c2b117644b9adb648fa1a590
Rino Jose <rjose@ejorp.com>
2012-11-27 22:46:20 -0800

    Removed: Phony data Refactored: tightened up code using a list comprehension

--- dovetail/projects/db.py
+++ dovetail/projects/db.py
@@ -5,46 +5,16 @@ import dovetail.people.models as people
 
 def select_project_collection(connection):
     data = connection.execute(database.projects.select())
-    result = []
-    for row in data:
-        p = {
-                'project_id': row['id'],
-                'title': row['name'],
-                'target_date': database.format_date(row['target_date']),
-                'est_date': database.format_date(row['est_end_date']),
-                'detail_url': '/projects/%d' % row['id'],
-                'key_dates': work.get_key_work_for_project(connection, row['id'])
-            }
-        result.append(p)
-    return result + get_phony_project_data()
+    result = [{
+            'project_id': row['id'],
+            'title': row['name'],
+            'target_date': database.format_date(row['target_date']),
+            'est_date': database.format_date(row['est_end_date']),
+            'detail_url': '/projects/%d' % row['id'],
+            'key_dates': work.get_key_work_for_project(connection, row['id'])
+            } for row in data]
+    return result
 
-# TODO: Delete this
-def get_phony_project_data():
-    search_project = {'project_id': 1,
-        'title': 'Search',
-        'target_date': 'Feb 15, 2012',
-        'est_date': 'Feb 10, 2012',
-        'detail_url': '/projects/1',
-        'key_dates': [['Integration with PAL', 'Dec 15, 2012']]}
-    endorsements_project = {'project_id': 2,
-        'title': 'Endorsements',
-        'target_date': 'Mar 15, 2012',
-        'est_date': 'Mar 20, 2012',
-        'detail_url': '/projects/2',
-        'key_dates': []}
-    rich_media_project = {'project_id': 3,
-        'title': 'Rich Media',
-        'target_date': 'Mar 15, 2012',
-        'est_date': 'Mar 2, 2012',
-        'detail_url': '/projects/3',
-        'key_dates': []}
-    mentions_project = {'project_id': 4,
-        'title': 'Mentions',
-        'target_date': 'Mar 25, 2012',
-        'est_date': 'Mar 25, 2012',
-        'detail_url': '/projects/4',
-        'key_dates': []}
-    return [search_project, endorsements_project, rich_media_project, mentions_project]
 
 def select_project(connection, project_id):
     data = connection.execute(database.projects.select(
@@ -62,28 +32,3 @@ def select_project(connection, project_id):
         'work': work.get_work_for_project(connection, project_id)
         }
     return result
-
-def get_phony_project_details(project_id):
-    rino = {'name': 'Rino Jose',
-            'picture': 'https://m1-s.licdn.com/mpr/mpr/shrink_80_80/p/2/000/019/20e/2464c31.jpg'}
-    result = {
-        'project_id': 2,
-        'name': 'Endorsements',
-        'stats': {
-            'target_date': 'Mar 15, 2012',
-            'est_date': 'Mar 20, 2012',
-            'total_effort': '40 man-days',
-            },
-        'participants': [
-            {'name': 'Rino Jose', 'title': "Engineering Manager", 'team': 'Mobile'}
-            ],
-        'work': [
-            {'name': 'Figure out the thing for the thing', 'is_done': 'Done: Oct 14, 2012',
-                'assignee': rino},
-            {'name': 'Do the actual work', 'assignee': rino},
-            {'name': 'Do more work', 'key_date': 'Dec 19, 2012', 'assignee': rino},
-            ]
-        }
-    return result
-
-
