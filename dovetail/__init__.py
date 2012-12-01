d7de70ea3f5b7d22d6be7b4668d590533a99b2b6
Rino Jose <rjose@ejorp.com>
2012-11-30 22:43:34 -0800

    Added: Serving of assets Added: jquery and bootstrap assets

--- dovetail/__init__.py
+++ dovetail/__init__.py
@@ -6,6 +6,8 @@ import dovetail.database as database
 import dovetail.projects.controller
 import dovetail.people.controller
 import dovetail.work.controller
+import os
+from werkzeug.wsgi import SharedDataMiddleware
 
 database.metadata.create_all()
 
@@ -19,14 +21,22 @@ def open_connection():
 def close_connection(exeption=None):
     g.connection.close()
 
+
 @app.route('/')
 def root():
     return redirect('/projects')
+
+# Static files
+
 
 # Register blueprints
 app.register_blueprint(dovetail.people.controller.mod)
 app.register_blueprint(dovetail.projects.controller.mod)
 app.register_blueprint(dovetail.work.controller.mod)
+
+# Register middleware
+app.wsgi_app = SharedDataMiddleware(app.wsgi_app, {
+    '/assets': os.path.join(os.path.dirname(__file__), 'assets')})
 
 if __name__ == '__main__':
     # TODO: Figure out how to set this via an environment variable
