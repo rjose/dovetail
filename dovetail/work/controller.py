3ccbcbdd1d2019a099d092ba3ab82ed0e0230eb3
Rino Jose <rjose@ejorp.com>
2012-12-01 20:44:16 -0800

    Added: can add work to project from modal

--- dovetail/work/controller.py
+++ dovetail/work/controller.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template, request, redirect, url_for, g
+from flask import (Blueprint, Response, json, render_template, request,
+        redirect, url_for, g)
 from datetime import datetime
 
 import dovetail.database as database
@@ -29,4 +30,21 @@ def work_new():
     return render_template('work/new.html', 
             project = project,
             people = people_db.select_people(g.connection))
+
+@mod.route('/api/work', methods=['POST'])
+def api_work():
+    # Condition data from request
+    title = request.values['title']
+    assignee_id = int(request.values['assignee_id'])
+    effort_left_d = float(request.values['effort_left_d'])
+    project_id = int(request.values['project_id'])
+
+    insert_result = g.connection.execute(database.work.insert(),
+           title = title,
+           assignee_id = assignee_id,
+           project_id = project_id,
+           effort_left_d = effort_left_d)
+
+    response_data = {'work_id': insert_result.inserted_primary_key}
+    return Response(json.dumps(response_data), status=200, mimetype='application/json')
 
